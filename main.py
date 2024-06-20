d2f2a6dec15fa7a68b072fcd06a3382e5727edf6
arturix2 <123446162+arturix2@users.noreply.github.com>
2024-06-20 18:11:01 +0000

    20.06.2024 - Flask

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello world"
+    return render_template('index.html')
+
+@app.route('/par_mums')
+def par_mums():
+    return render_template('par_mums.html')
+
+@app.route('/kontakti')
+def kontakti():
+    return render_template('kontakti.html')
+
+@app.route('/aiziet', methods=['POST'])
+def aiziet():
+    lietotajs = request.form['lietotajvards']
+    return f"Paldies, {lietotajs}! Jūsu pieteikums ir saņemts"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
